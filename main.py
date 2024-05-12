b872b0b30202a3350d79e8dc527e94d50321eae8
Vladimir Kardashin <kardv@yandex.ru>
2024-05-12 20:51:07 +0300

    изменил код ветки new-test

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 # Ветка new-test
-for i in range(8):
-    print("слово")
+for i in range(9):
+    print("slovo")
 
-a, b = 9, 8
-
-if a > b:
-    print("a больше чем b")
-elif b > a:
-    print("b больше чем a")
-else:
-    print("a равно b")
