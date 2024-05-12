9aedbd8e1baf174358242f33f9eec6698a5fe3fe
Vladimir Kardashin <kardv@yandex.ru>
2024-05-12 20:34:26 +0300

    new code for in new-test

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-# Ветка main
-print("Конфликтная строка")
+# Ветка new-test
+for i in range(8):
+    print("слово")
+
+a, b = 9, 8
+
+if a > b:
+    print("a больше чем b")
+elif b > a:
+    print("b больше чем a")
+else:
+    print("a равно b")
